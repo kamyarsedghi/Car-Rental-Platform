c8db444c37cb5e4343afb83863917845c1c89cec
Kamyar Sedghi <kamyarsedghimoghadam@gmail.com>
2023-02-16 17:27:49 +0400

    Updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,18 @@ import { CarService } from './car/car.service';
 import { CarController } from './car/car.controller';
 import { CarModule } from './car/car.module';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './utils/database/database.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-    CarModule,
-  ],
-  controllers: [CarController],
-  providers: [CarService],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+            isGlobal: true,
+        }),
+        CarModule,
+        DatabaseModule,
+    ],
+    controllers: [CarController],
+    providers: [CarService],
 })
 export class AppModule {}
