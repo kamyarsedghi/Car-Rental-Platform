0552351c657baad1394479b7c442c729f177b3e9
Kamyar Sedghi <kamyarsedghimoghadam@gmail.com>
2023-02-17 11:38:14 +0400

    Added Reservation module and service

--- src/car/car.module.ts
+++ src/car/car.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ReservationModule } from 'src/reservation/reservation.module';
+import { ReservationService } from 'src/reservation/reservation.service';
 import { DatabaseModule } from 'src/utils/database/database.module';
 import { DatabaseService } from 'src/utils/database/database.service';
 import { CarController } from './car.controller';
 import { CarService } from './car.service';
 
 @Module({
-    imports: [DatabaseModule],
+    imports: [DatabaseModule, ReservationModule],
     controllers: [CarController],
-    providers: [CarService, DatabaseService],
+    providers: [CarService, DatabaseService, ReservationService],
+    exports: [CarService],
 })
 export class CarModule {}
